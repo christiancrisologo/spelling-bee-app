10ce199f76a4942c3c9efba75cf484408f2f2f61
christian-crisologo-lrn <christian.crisologo@learnosity.com>
2024-06-09 10:21:11 +1000

    creat a dummy json

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { fetchWords } from "./lib/data";
 import Main from "./ui/Main";
+import data from './data.json';
 
 export default async function Home() {
-  const words = await fetchWords();
+  const words = data;
 
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-between">
