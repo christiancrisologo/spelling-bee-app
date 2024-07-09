06b12d114753536ae41b1e1f03a7d6f4208d5993
christian crisologo <ccrisologo@christian-crisologo.local>
2024-07-09 12:44:48 +1000

    remove expert level

--- app/ui/HomeScreen.tsx
+++ app/ui/HomeScreen.tsx
@@ -1,8 +1,8 @@
-"use client"
+'use client'
 
-import React, {useCallback, useState, Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import OptionGroup, { OptionProps } from '../bee/ui/OptionGroup';
+import React, { useCallback, useState, Suspense } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import OptionGroup, { OptionProps } from '../bee/ui/OptionGroup'
 
 export type UserSettings = {
   totalWords: number
@@ -13,73 +13,76 @@ export type UserSettings = {
 
 const levelOptions: OptionProps[] = [
   {
-      label: "junior",
-  },{
-      label: "senior",
-  },{
-    label: "expert",
-  }
-  
-];
+    label: 'elementary',
+  },
+  {
+    label: 'junior',
+  },
+  {
+    label: 'senior',
+  },
+]
 
 const StartScreenComponent = () => {
-  const searchParams = useSearchParams()!;
-  const router = useRouter();
-  const [playerName, setPlayerName] = useState('');
-  const [level, setLevel] = useState('junior');
+  const searchParams = useSearchParams()!
+  const router = useRouter()
+  const [playerName, setPlayerName] = useState('')
+  const [level, setLevel] = useState('junior')
 
+  const onSubmit = useCallback(() => {
+    const params = new URLSearchParams(searchParams)
+    params.set('playerName', playerName)
+    params.set('level', level)
 
-  const onSubmit = useCallback(
-    () => {
-      const params = new URLSearchParams(searchParams);
-      params.set('playerName', playerName);
-      params.set('level', level);
-      
-      router.push('/bee' + '?' + params.toString());  
-    },
-    [level, playerName, router, searchParams],
-  );
+    router.push('/bee' + '?' + params.toString())
+  }, [level, playerName, router, searchParams])
 
   const handleKeyDown = (event: any) => {
     if (event.keyCode === 13) {
-      onSubmit();
+      onSubmit()
     }
+  }
 
-};
-
-  return (<div className="flex flex-col justify-center"> 
+  return (
+    <div className="flex flex-col justify-center">
       <div className="flex mt-2 justify-center flex-col md:flex-row mx-6">
-        <input type="text" name="word-input" id="word-input" 
-          className="bg-white p-4 rounded-xl text-xl font-bold md:me-2" 
+        <input
+          type="text"
+          name="word-input"
+          id="word-input"
+          className="bg-white p-4 rounded-xl text-xl font-bold md:me-2"
           placeholder="What's your name?"
           value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)} />
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
         <button
           className="mt-1 p-4 bg-blue-500 text-white hover:bg-blue-700 focus:outline-none text-xl font-bold rounded-xl disabled:bg-gray-400"
           onClick={onSubmit}
           onKeyDown={handleKeyDown}
           disabled={!playerName.length}
-          >
-            Submit
+        >
+          Submit
         </button>
       </div>
       <div className="flex mt-2 justify-center flex-col md:flex-row mx-6">
-          <OptionGroup 
-            className="self-center"
-            label="Level"
-            options={levelOptions}
-            onChange={(value: string) => {
-              setLevel(value);
-            }}
-            selected={level}
-          />
+        <OptionGroup
+          className="self-center"
+          label="Level"
+          options={levelOptions}
+          onChange={(value: string) => {
+            setLevel(value)
+          }}
+          selected={level}
+        />
       </div>
-      
-  </div>);
-};
+    </div>
+  )
+}
 
-export default function StartScreen () {
-  return <Suspense>
-    <StartScreenComponent />
+export default function StartScreen() {
+  return (
+    <Suspense>
+      <StartScreenComponent />
     </Suspense>
-};
+  )
+}
